6892eac2107eef25516e5edef6a87b87175b29ef
charitie <charitytarus21@gmail.com>
2020-06-22 01:24:56 +0300

    Implement Login

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import axios from 'axios';
-import { connect } from 'mongoose';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -19,27 +20,7 @@ const Register = () => {
     if(password !== password2) {
       console.log('password don\'t match')
     } else {
-      const newUser = {
-        name,
-        email,
-        password
-      }
-
-      try {
-        const config = {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
-
-        const body = JSON.stringify(newUser);
-
-        const res = await axios.post('/users', body, config);
-        console.log(res.data);
-        
-      } catch (err) {
-        console.log(err.response.data);
-      }
+      console.log('registration successful')
     }
   }
 
@@ -95,7 +76,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   )
